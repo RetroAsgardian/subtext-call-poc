cf9ec38ebadb4858ee6944136800dc3579f64144
RetroAsgardian <oskarsmacaw@gmail.com>
2020-03-25 04:06:12 -0400

    x

--- call.js
+++ call.js
@@ -1,28 +1,33 @@
 var peer = new Peer();
+var myStream = null;
+navigator.mediaDevics.getUserMedia({audio: true, video: true}).then(function(s) {
+	myStream = s;
+	document.querySelector('#myVideo').src = URL.createObjectURL(myStream);
+	document.querySelector('#myVideo').play();
+});
 
 peer.on('open', function(id) {
 	document.getElementById("myId").innerHTML = "My ID: " + id;
 });
 
 peer.on('call', function(call) {
-	document.getElementById("myId").innerHTML = "My ID: " + peer.id + "[CALL IN: " + call.peer + "]";
-	navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(myStream) {
-		call.answer(myStream);
-		call.on('stream', function(theirStream) {
-			document.querySelector('#callVideo').src = URL.createObjectURL(theirStream);
-			document.querySelector('#callVideo').play();
-		});
+	document.getElementById("myId").innerHTML = "My ID: " + peer.id + " [IN: " + call.peer + "]";
+	call.answer(myStream);
+	call.on('stream', function(theirStream) {
+		document.querySelector('#callVideo').src = URL.createObjectURL(theirStream);
+		document.querySelector('#callVideo').play();
 	});
 });
 
 function initiateCall() {
 	var target = document.querySelector('#callTargetId').value;
-	navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(myStream) {
-		call = peer.call(target, myStream);
-		document.getElementById("myId").innerHTML = "My ID: " + peer.id + "[CALL OUT: " + target + "]";
-		call.on('stream', function(theirStream) {
-			document.querySelector('#callVideo').src = URL.createObjectURL(theirStream);
-			document.querySelector('#callVideo').play();
-		});
+	call = peer.call(target, myStream);
+	document.getElementById("myId").innerHTML = "My ID: " + peer.id + " [OUT: " + target + "]";
+	call.on('answer', function() {
+		document.getElementById("myId").innerHTML = "My ID: " + peer.id + " [OUT: " + target + " ANS]";
+	});
+	call.on('stream', function(theirStream) {
+		document.querySelector('#callVideo').src = URL.createObjectURL(theirStream);
+		document.querySelector('#callVideo').play();
 	});
 }
