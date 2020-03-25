cfff88ecfc363db5b3725e2615a05d3a82b702cd
RetroAsgardian <oskarsmacaw@gmail.com>
2020-03-25 04:07:29 -0400

    x

--- call.js
+++ call.js
@@ -1,6 +1,6 @@
 var peer = new Peer();
 var myStream = null;
-navigator.mediaDevics.getUserMedia({audio: true, video: true}).then(function(s) {
+navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(s) {
 	myStream = s;
 	document.querySelector('#myVideo').src = URL.createObjectURL(myStream);
 	document.querySelector('#myVideo').play();
@@ -23,9 +23,6 @@ function initiateCall() {
 	var target = document.querySelector('#callTargetId').value;
 	call = peer.call(target, myStream);
 	document.getElementById("myId").innerHTML = "My ID: " + peer.id + " [OUT: " + target + "]";
-	call.on('answer', function() {
-		document.getElementById("myId").innerHTML = "My ID: " + peer.id + " [OUT: " + target + " ANS]";
-	});
 	call.on('stream', function(theirStream) {
 		document.querySelector('#callVideo').src = URL.createObjectURL(theirStream);
 		document.querySelector('#callVideo').play();
